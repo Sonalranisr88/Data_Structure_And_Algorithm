f9e1d741818389de2bce6ccecd7dcda280b9bdcd
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-08-16 23:27:57 +0530

    Add files via upload

---
+++ Array.cpp
@@ -0,0 +1,14 @@
+#include<bits/c++io.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+    int  a[5];
+    int  b[5] ={2,4,6,8,9};
+
+    for(int i : a){
+
+        cout<<sizeof(a)<<endl;
+        cout<<b[i]<<endl;;
+    };
+}
