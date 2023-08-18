5ac4e9ab8260f08d3b5df05ea841ace8c9b1e0df
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-08-18 23:23:28 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,21 @@
+#include<bits/c++io.h>
+#include<iostream>
+
+using namespace std;
+
+int add(int a,int b)
+{
+    int c;
+    c=a+b;
+
+    return c;
+}
+
+int main(){
+    int num1= 10 , num2 =90 , sum;
+    sum = add(num1,num2);
+
+    cout<<"The sum of two number is: "<<sum<<endl;
+    return 0;
+}
+
