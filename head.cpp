33bd3e39417c803e76ddf3c0a67e5cebf427c169
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-08-19 22:39:55 +0530

    Add files via upload

---
+++ head.cpp
@@ -0,0 +1,19 @@
+#include<bits/c++io.h>
+#include<iostream>
+
+using namespace std;
+
+void fun(int n)
+{
+    if(n>0)
+    {
+        fun(n-1);
+        cout<<n<<endl;
+        
+    }
+}
+int main(){
+    int x=3;
+    fun(x);
+    return 0;
+}
