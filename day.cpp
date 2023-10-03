4952fb655143fb1fcdf8203c7ee3c5b458470f25
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-10-03 23:39:51 +0530

    Add files via upload

---
+++ day.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+int day;
+cin>>day;
+switch(day){
+    case 1:
+    cout<<"Monday"<<endl;
+    break;
+    case 2:
+    cout<<"Tuesday"<<endl;
+    break;
+    case 3:
+    cout<<"Wednesday"<<endl;
+    break;
+    case 4:
+    cout<<"Thrusay"<<endl;
+    break;
+    case 5:
+    cout<<"Friay"<<endl;
+    break;
+    case 6:
+    cout<<"Saturday"<<endl;
+    break;
+    case 7:
+    cout<<"Sunday"<<endl;
+}
+    return 0;
+}
