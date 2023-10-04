bcec2e006794699fb4bb63af23a98003e8e95459
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-10-04 21:59:42 +0530

    Add files via upload

---
+++ Pattern3.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(int n){
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    print(n);
+}
