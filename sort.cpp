0e57df5c764c4bdaa348ea86fd2d0da219205cbc
Sonal Rani <104574845+Sonalranisr88@users.noreply.github.com>
2023-10-05 22:14:04 +0530

    Add files via upload

---
+++ sort.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sortexp(){
+    vector<int>v;
+    v.push_back(23);
+    v.push_back(56);
+    v.push_back(11);
+    v.push_back(99);
+    v.push_back(45);
+    
+    cout<<sort(v.begin(),v.end());
+}
+int main(){
+    return 0;
+}
